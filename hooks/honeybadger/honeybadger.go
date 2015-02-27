c76001942e542dbdfe0d34b60c1a7d341f157845
Álex González <agonzalezro@gmail.com>
2015-02-27 14:34:37 +0000

    Add Honeybadger.com hook
    This hook is using agonzalezro/goneybadger as client.

---
+++ hooks/honeybadger/honeybadger.go
@@ -0,0 +1,44 @@
+package logrus_honeybadger
+
+import (
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/agonzalezro/goneybadger"
+)
+
+// HoneybadgerHook to send exceptions to an exception-tracking service compatible
+// with the Honeybadger API.
+type HoneybadgerHook struct {
+	apiKey, env string
+	timeout     time.Duration
+}
+
+func NewHoneyBadgerHook(apiKey, env string, timeout time.Duration) *HoneybadgerHook {
+	return &HoneybadgerHook{
+		apiKey:  apiKey,
+		env:     env,
+		timeout: timeout,
+	}
+}
+
+func (h *HoneybadgerHook) Fire(entry *logrus.Entry) error {
+	gb := goneybadger.NewWithTimeout(h.apiKey, h.env, h.timeout)
+	err := gb.Notify(entry.Message)
+	if err != nil {
+		entry.Logger.WithFields(logrus.Fields{
+			"source": "honeybadger",
+			"error":  err,
+		}).Warn("Failed to send error to HoneyBadger")
+	}
+
+	return nil
+}
+
+func (h *HoneybadgerHook) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.ErrorLevel,
+		logrus.FatalLevel,
+		logrus.PanicLevel,
+	}
+}
